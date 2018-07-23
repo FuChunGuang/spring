9873b74f09156ce8f20548cbcc6ab8b72ff2b3cd
FuChunGuang <FuChunGuang@users.noreply.github.com>
2018-07-23 20:31:23 +0800

    小调整v0.01

--- crud/src/main/java/com/spring/fcg/controller/HelloController.java
+++ crud/src/main/java/com/spring/fcg/controller/HelloController.java
@@ -3,11 +3,18 @@ package com.spring.fcg.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.fcg.entity.User;
+import com.spring.fcg.util.BaseController;
+
 @Controller
-public class HelloController {
+public class HelloController extends BaseController{
 	@RequestMapping("/hello")
 	public String hello() {
 		return "html/hello";
@@ -22,9 +29,17 @@ public class HelloController {
 		
 		return "html/login";
 	}
-	@RequestMapping("/index")
-	public String index() {
-		
+	@PostMapping("/index")
+	public String index(@Valid User user,BindingResult bindingResult) {
+		Map<String,Object> map=getParam();
+		System.out.println(map.get("username"));
+		System.out.println(map.get("password"));
+		System.out.println(user.getUsername());
+		System.out.println(user.getPassword());
+	    if (bindingResult.hasErrors()){
+	        System.out.println(bindingResult.getFieldError().getDefaultMessage());
+	        return "html/login";
+	    }
 		return "html/index";
 	}
 }
