16562aada8d90d3910c8d3c32c10cc7798c4d351
FuChunGuang <FuChunGuang@users.noreply.github.com>
2018-08-09 19:05:03 +0800

    version-1.0

--- crud/src/main/java/com/spring/fcg/controller/HelloController.java
+++ crud/src/main/java/com/spring/fcg/controller/HelloController.java
@@ -1,30 +1,21 @@
 package com.spring.fcg.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/hello")
-	public String hello() {
-		return "html/hello";
-	}
-	@RequestMapping("/demo")
-	public String demo(Map<String, String> map) {
-		map.put("demo", "11111");
-		return "demo";
-	}
-	@RequestMapping("/login")
-	public String login() {
-		
-		return "html/login";
-	}
+
 	@RequestMapping("/index")
 	public String index() {
-		
+
 		return "html/index";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+
+		return "html/login";
 	}
 }
